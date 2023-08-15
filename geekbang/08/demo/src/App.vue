26dfc4d33a5ef8806d44b447a5161641208db4ab
pencilCool <yhtangcoder@gmail.com>
2023-08-15 14:33:00 +0800

    chapter 8 done

---
+++ geekbang/08/demo/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Rate from "./components/Rate.vue";
+import { ref } from "vue";
+let score = ref(3.5);
+</script>
+
+<template>
+  <h1>your score is {{ score }}</h1>
+  <Rate v-model="score">课程评分</Rate>
+  <Rate v-model="score">
+    <img width="14" src="/favicon.ico" />
+  </Rate>
+</template>
---
+++ geekbang/08/demo/src/components/Rate.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :style="fontstyle">
+    <slot></slot>
+    <div class="rate" @mouseout="mouseOut">
+      <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+      <span class="hollow" :style="fontwidth">
+        <span
+          @click="onRate(num)"
+          @mouseover="mouseOver(num)"
+          v-for="num in 5"
+          :key="num"
+          >★</span
+        >
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+let props = defineProps({
+  modelValue: Number,
+  theme: { type: String, default: "orange" },
+});
+
+let emits = defineEmits("update:modelValue");
+function onRate(num) {
+  emits("update:modelValue", num);
+}
+
+const themeObj = {
+  black: "#00",
+  white: "#fff",
+  red: "#f5222d",
+  orange: "#fa541c",
+  yellow: "#fadb14",
+  green: "#73d13d",
+  blue: "#40a9ff",
+};
+
+const fontstyle = computed(() => {
+  return `color:${themeObj[props.theme]};`;
+});
+
+let width = ref(props.value);
+
+function mouseOver(i) {
+  width.value = i;
+}
+
+function mouseOut() {
+  width.value = props.modelValue;
+}
+
+const fontwidth = computed(() => `width:${width.value}em;`);
+</script>
+
+<style scoped>
+.rate {
+  position: relative;
+  display: inline-block;
+}
+.rate > span.hollow {
+  position: absolute;
+  display: inline-block;
+  top: 0;
+  left: 0;
+  width: 0;
+  overflow: hidden;
+}
+</style>
