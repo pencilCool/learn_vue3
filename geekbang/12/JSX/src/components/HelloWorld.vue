7a41ac0fe423fb1f9eea00b0de472b9e86647151
pencilCool <yhtangcoder@gmail.com>
2023-08-17 16:25:37 +0800

    chapter 12 jsx

---
+++ geekbang/12/JSX/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <Heading :level="1">hello geekbang</Heading>
+  <Todo></Todo>
+</template>
+
+<script setup>
+import Heading from './Heading.jsx'
+import Todo from './Todo.jsx'
+</script>
